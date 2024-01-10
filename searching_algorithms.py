854abf6436dfb68d6893e1a9ee1b7515260b91ef
Frances <135648147+fancypog@users.noreply.github.com>
2024-01-10 13:24:38 +0000

    Update searching_algorithms.py

--- searching_algorithms.py
+++ searching_algorithms.py
@@ -25,3 +25,20 @@ def binarySearch(nums, target, l, r):
     return binarySearch(nums, target, 0, len(nums) - 1)
 
 binarySearch(l1, 5, 0, len(l1))
+
+
+
+# In binary search, you can keep an eye of the boundry without using a real array
+# Example: guess number higher or lower (Leetcode problem)
+def guessNumber(self, n: int) -> int:
+    l, r = 1, n
+    
+    while True:
+        m = (l + r) // 2
+        result = guess(m)
+        if result > 0:
+            l = m + 1
+        elif result < 0:
+            r = m - 1
+        else:
+            return m
